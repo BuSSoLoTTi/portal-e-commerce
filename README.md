84873a0251bf6808592704b7574727b7bb50e24d
BuSSoLoTTi <matheusbussolotti@gmial.com>
2023-06-06 13:53:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# diw-portal-ecommerce
+Portal de e-commerce utilizando a [Fake Store API](https://github.com/keikaavousi/fake-store-api).
+
+Trabalho da disciplina Desenvolvimento de Interfaces Web do curso de Engenharia de Software da PUC Minas.
---
+++ script/index.js
@@ -0,0 +1,95 @@
+import * as Utils from './utils.js'
+
+// Categories Dropdown
+
+fetch(Utils.API_URL + '/products/categories')
+    .then(res => res.json())
+    .then(categories => {
+
+        for (const category of categories) {
+
+            document.getElementById('categories-select').innerHTML += `
+                        <option value="${category}">${category}</option>
+                    `;
+
+        }
+
+    });
+
+// Products Showcase & Products Featured
+
+fetch(Utils.API_URL + '/products?limit=12')
+    .then(res => res.json())
+    .then(products => {
+
+        console.log(products);
+
+        let sidebarLimit = 6;
+        let productsShowcase = document.getElementById('products-showcase');
+
+        productsShowcase.innerHTML = "";
+
+        for (const product of products) {
+
+            let href = Utils.getProductHref(product.id);
+            let starsHTML = Utils.getStarsHTML(product.rating.rate);
+            let price = Utils.getFormattedPrice(product.price);
+
+            productsShowcase.innerHTML += `
+                <div class="col-sm-12 col-md-4 mb-4">
+                    <div class="card">
+                    <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
+                        <a href="${href}">
+                            <img 
+                                src="${product.image}" 
+                                class="card-img-top d-block m-auto h-auto w-auto p-3 products-featured-img"
+                            >
+                        </a>
+                        </div>
+                        <div class="card-body">
+                            <a href="${href}" class="text-decoration-none">    
+                                <h5 class="card-title text-truncate">${product.title}</h5>
+                            </a>
+                            <p class="text-truncate">${product.description}</p>
+                            <div class="text-center">
+                                ${starsHTML}
+                                <p class="fs-5 fw-bold">${price}</p>
+                            </div>
+                            <a href="${href}" class="text-decoration-none">
+                                <div class="d-grid">
+                                        <button class="btn text-uppercase fw-bold">Detalhes</button>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>`;
+
+            if (sidebarLimit > 0) {
+
+                document.getElementById('products-featured').innerHTML += `
+                        <div class="card mb-2">
+                            <div class="row card-body">
+                                <div class="col-sm-4">
+                                    <div class="d-flex justify-content-center align-items-center" style="height: 100px;">
+                                        <a href="${href}">
+                                            <img src="${product.image}" class="products-sidebar-img">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-sm-8">
+                                    <a href="${href}" class="text-decoration-none">
+                                        <h6 class="card-title text-truncate">${product.title}</h6>
+                                    </a>
+                                    ${starsHTML}
+                                    <h6>${price}</h6>
+                                </div>
+                            </div>
+                        </div>`;
+
+                sidebarLimit--;
+
+            }
+
+        }
+
+    });
